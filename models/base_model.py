8b50350e718692faa63045fedb7408bb80955d63
Codart-7 <iconmoa@gmail.com>
2022-08-06 19:29:59 +0100

    [fix] edit base_model

--- models/base_model.py
+++ models/base_model.py
@@ -25,22 +25,23 @@ class BaseModel():
         date_string: time format
         """
         date_string = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
 
-        if (len(kwargs) != 0):
+        if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     """
                     created_at and updated_at is changed to
                     date time object using strptime
                     """
-                    self.__dict__[key] = datetime.strptime(value, date_string)
+                    value = datetime.strptime(kwargs[key], date_string)
 
                 else:
                     """returns the datetime as string"""
                     self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """
