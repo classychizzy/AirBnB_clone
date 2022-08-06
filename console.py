f54f07847777157e852e082fad702fe5ed75f56f
Codart-7 <iconmoa@gmail.com>
2022-08-06 09:42:54 +0100

    [update] console show feature

--- console.py
+++ console.py
@@ -1,19 +1,62 @@
 #!/usr/bin/python3
 """comment"""
 import cmd
+from models.base_model import BaseModel
+import json
 
 class HBNBCommand(cmd.Cmd):
-    """comment"""
-    prompt = '(hbnb)'
+    """Defines the AirBnB command interpreter.
 
-    def do_quit(self):
-        """This command is to quit"""
-        self.close()
+    Attributes:
+        prompt (str): The command prompt.
+    """
+
+    prompt = "(hbnb) "
+    classDict = {'BaseModel': BaseModel}
+
+    def do_quit(self, arg):
+        """Quit command to exit the programm"""
         return True
 
-    def emptyline():
+    def emptyline(self):
+        """An empty line does nothing"""
         pass
 
+    def do_EOF(self, arg):
+        """Quits the console"""
+        return True
+
+    def do_create(self, model):
+        """Creates a new instance of a given class, saves it and prints the id.
+        Usage: create <className>
+        """
+        
+        if not model:
+            print("** class name missing **")
+        elif model not in HBNBCommand.classDict.keys():
+            print("** class doesn't exist **")
+        else:
+            my_model = HBNBCommand.classDict[model]()
+            print(my_model.id)
+            #my_model.save()
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance based on the class name and id.
+        Usage: show <className> <instanceID>
+        """
+        
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split(' ')
+        
+        if args[0] not in HBNBCommand.classDict.keys():
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            print("test")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
